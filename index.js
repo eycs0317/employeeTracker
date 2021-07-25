89f992beb29826f69bb2d1a72ee44af347b982a8
Eddie Yeung <eycs0317@gmail.com>
2021-07-25 15:01:49 -0700

    showallDept function working

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const connection = mysql.createConnection({
 //     inner join role r on e.role_id = r.id
 // WHERE e.role_id = 2
 
-
+function showAllDept() {
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) throw err;
+    console.table(results)
+  })
+}
+showAllDept();
 function showAllEmpl() {
   var str = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.name
               FROM employee e
