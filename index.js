d0e31a5af68fa14e6e383b373b33c5bb8b3c2164
Eddie Yeung <eycs0317@gmail.com>
2021-07-25 15:38:42 -0700

    missing manager name on showallempl

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const connection = mysql.createConnection({
 });
 
 
-// SELECT e.* r.*
-// FROM employee e
-//     inner join role r on e.role_id = r.id
-// WHERE e.role_id = 2
+
+
+
+
+
+
+
+
+
 
 function showAllDept() {
   connection.query('SELECT * FROM department', (err, results) => {
@@ -22,16 +27,35 @@ function showAllDept() {
     console.table(results)
   })
 }
-showAllDept();
+// showAllDept()
+
+function showAllRole() {
+  var str = `SELECT r.id, r.title, r.salary, d.name
+            FROM role r
+            inner join department d on r.department_id = d.id;`
+  connection.query(str, (err, results) => {
+    if (err) throw err;
+    console.table(results)
+  })
+}
+
+// showAllRole();
+
+
+//missing manager name
 function showAllEmpl() {
-  var str = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.name
+  var str = `SELECT e.id, e.first_name, e.last_name, role.title, role.salary, d.name, e.manager_id, m.manager_id
               FROM employee e
-              inner join role r on e.role_id = r.id
-              inner join department d on r.department_id = d.id;`
+              inner join role on e.role_id = role.id
+              inner join department d on role.department_id = d.id
+              inner join employee m on m.manager_id = e.id;
+              `
   connection.query(str, (err,results) => {
     if (err) throw err;
     console.log('results', results)
     console.table(results)
   })
 }
+
+// showAllEmpl();
 
